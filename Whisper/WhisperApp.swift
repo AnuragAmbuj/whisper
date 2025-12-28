ef44e431819f1e52bcd21b6cf079c9e71cd4667f
AnuragAmbuj <anuragambuj000@gmail.com>
2025-12-28 22:55:14 +0530

    Initial Commit

---
+++ Whisper/WhisperApp.swift
@@ -0,0 +1,32 @@
+//
+//  WhisperApp.swift
+//  Whisper
+//
+//  Created by Anurag Ambuj on 28/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WhisperApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ WhisperTests/WhisperTests.swift
@@ -0,0 +1,16 @@
+//
+//  WhisperTests.swift
+//  WhisperTests
+//
+//  Created by Anurag Ambuj on 28/12/25.
+//
+
+import Testing
+
+struct WhisperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
